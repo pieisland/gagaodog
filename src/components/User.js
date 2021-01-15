1a708277b5457bbca83e73463ff8a030db1add95
pieisland <pieisland@naver.com>
2021-01-16 08:58:14 +0900

    feat : [FE] 프로필 이미지 적용, 배경색 변경
    - 프로필 이미지를 적용함.
    - 맞지 않는 배경색 주석 처리.
    - css 수정.

--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import profile from "../../public/images/neoul.png";
 
 const UserWrap = styled.div`
   width: 100%;
   height: 70px;
-  background-color: brown;
+  //background-color: brown;
 
   display: flex;
   justify-content: center;
@@ -14,24 +15,39 @@ const UserWrap = styled.div`
 
 const UserInnerWrap = styled.div`
   position: absoulte;
-  width: 95%;
+  width: 100%;
   height: 100%;
-  background-color: orange;
+  //background-color: orange;
   display: flex;
   align-items: center;
+
+  &:hover {
+    background-color: rgb(246, 246, 247);
+  }
 `;
 
 const ProfilePicture = styled.div`
   width: 50px;
   height: 50px;
-  border-radius: 10px;
+  border-radius: 18px;
   background-color: white;
+
+  background-image: url(${profile});
+  background-size: 50px;
+  margin-left: 20px;
 `;
 
 const ProfileContent = styled.div`
   width: 100%;
-  height: 50%;
-  background-color: skyblue;
+  height: 80%;
+  //background-color: skyblue;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  > div {
+    margin-left: 10px;
+  }
 `;
 
 const User = () => {
@@ -40,7 +56,10 @@ const User = () => {
       <UserWrap>
         <UserInnerWrap>
           <ProfilePicture></ProfilePicture>
-          <ProfileContent>안녕하세요</ProfileContent>
+          <ProfileContent>
+            <div>너울</div>
+            <div>안녕하세요</div>
+          </ProfileContent>
         </UserInnerWrap>
       </UserWrap>
     </>
